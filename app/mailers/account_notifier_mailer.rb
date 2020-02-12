8dc77b74356193f6c1016248e32b97062fa2d48a
David Gomez <davdg25@gmail.com>
2020-02-12 08:06:53 -0700

    create mailer that pulls in info

---
+++ app/mailers/account_notifier_mailer.rb
@@ -0,0 +1,7 @@
+class AccountNotifierMailer < ApplicationMailer
+
+  def inform(user, user_contact)
+    @user = user
+    mail(to: user_contact, subject: "#{user.first_name} Account Created.")
+  end
+end
